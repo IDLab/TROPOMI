031cb7894481e875cb9be37ba65970dd1146a2d8
Jasper van Vliet <jasper.van.vliet@ilent.nl>
2018-09-11 02:15:46 +0200

    Further improvement on filtering relevant ships

--- scripts/1_collectSingleShipObs.R
+++ scripts/1_collectSingleShipObs.R
@@ -3,113 +3,101 @@ library (RANN)
 library (dplyr)
 
 fCollectSingleShipObs <- function (NO2Obs, Shipdata, ShipName) {
-  #ShipName <- ShipNames[1]
-  
+  #ShipName <- ShipNames[12]
   
   SingleShipLoc <- filter(ShipData, Name == ShipName)
   nrow(SingleShipLoc)
- # if (nrow(SingleShipLoc)>1) {
-    #extract nearest neighbours observations of known AIS locations
-    matches <- nn2(NO2Obs[,1:2], SingleShipLoc[,2:3],1)
-    #combine Obs en Loc
-    SingleShipMatches <- cbind (SingleShipLoc,NO2Obs[matches$nn.idx,])
-    #Calculate differene in Time of datapoint
-    SingleShipMatches$DeltaT <- SingleShipMatches[,1]-SingleShipMatches[,11]
-    
-    # Select the rows with the lowest timedifference
-    best <- which.min(abs(SingleShipMatches$DeltaT))
-    nextbest <- nn2(SingleShipMatches$DeltaT, SingleShipMatches$DeltaT[best],2)$nn.idx[2]
-    
-    #build a df on the best available match for ship location
-    bestSingleShipLoc <- filter (SingleShipLoc, TimeStamp == SingleShipMatches$TimeStamp[best])
-    
-    #Interpolate to increase matching the right spot
-    
-    #build a data.frame for clarity
-    nrows <- 150
-    SingleShipInterpol <- as.data.frame(matrix(NA,nrows,3))
-    colnames(SingleShipInterpol) <- c("time", "Latitude", "Longitude")
-    
-    #make series to match from
-    SingleShipInterpol$time <- seq (SingleShipMatches$TimeStamp[best], SingleShipMatches$TimeStamp[nextbest], length.out = nrows)
-    SingleShipInterpol$Latitude <- seq (SingleShipMatches$Latitude[best], SingleShipMatches$Latitude[nextbest], length.out = nrows)
-    SingleShipInterpol$Longitude <- seq (SingleShipMatches$Longitude[best], SingleShipMatches$Longitude[nextbest], length.out = nrows)
-    
-    #find the best matching interpolated time
-    idx <- nn2(SingleShipInterpol$time, SingleShipMatches$time[best],1)$nn.idx
-    
-    #Start with original best match, update with new tim, lat and long
-    bestSingleShipLoc$TimeStamp <- SingleShipInterpol$time[idx]
-    bestSingleShipLoc$Latitude <-SingleShipInterpol$Latitude[idx]
-    bestSingleShipLoc$Longitude <- SingleShipInterpol$Longitude[idx]
-    # add Origin as opposite of Heading
-    bestSingleShipLoc$Origin <- ifelse(bestSingleShipLoc$Heading[1]>180,bestSingleShipLoc$Heading[1] - 180,bestSingleShipLoc$Heading[1] + 180)
-    
-    #clean up mess of intermediate vars
-    rm(nrows,best, nextbest, idx, SingleShipInterpol)
-    
-    ############
-    #Match surrounding cells with bestSingleShipLoc, calculate diff in concentrations and plot
-    
-    #first determine which obs are surrounding the ship
-    matches <- nn2(NO2Obs[,1:2], bestSingleShipLoc[,2:3],100)
-    SingleShipSurround <- NO2Obs[matches$nn.idx,]
-    SingleShipSurround$DistToOrg <- distGeo(cbind(SingleShipSurround$Longitude,SingleShipSurround$Latitude), c(bestSingleShipLoc$Longitude, bestSingleShipLoc$Latitude))/1000
-    #determine for each point the difference in Obs conc with the cell where the ship is at >0 means higher than where ship is. 
-    refConc <-  SingleShipSurround$Obs[which(SingleShipSurround$DistToOrg == min(SingleShipSurround$DistToOrg))]
-    SingleShipSurround$DeltaConc <- SingleShipSurround$Obs-refConc
-    # calculate the bearing of each cell surrounding the ship, to later assign sector in polar plot
-    origin <-c(bestSingleShipLoc$Longitude,bestSingleShipLoc$Latitude)
-    SingleShipSurround$Bearing <- sapply(1:nrow(SingleShipSurround), function (x) {bearingRhumb(origin,c(SingleShipSurround$Longitude[x],SingleShipSurround$Latitude[x]))})
-    
-    ##assign Polar sector and Distance sector to the Obs surrounding the ship
-    #YOU MUST USE AN EVEN NUMBER OF SECTORS
-    step <- 360/20
-    BearingSectors <- seq(step/2,360-step/2, by = step)
-    #modify sectors so that centre of a sector will match the ships heading 
-    #THIS IS NOT WORKING FOR THE FIRST AND LAST NORTHERN SECTOR YET
-    nearest <- nn2(BearingSectors, bestSingleShipLoc$Origin,2)
-    dif <- (BearingSectors[nearest$nn.idx[1]]-BearingSectors[nearest$nn.idx[2]])/2
-    BearingShift <- ifelse (dif >= 0, dif - nearest$nn.dists[1], dif + nearest$nn.dists[1])
-    BearingSectors <- BearingSectors + BearingShift
-    #define the sectors with respect to distance
-    DistanceSectors <- seq(min(SingleShipSurround$DistToOrg),max(SingleShipSurround$DistToOrg), length.out = 10)
-    #assign default value for Distance and Polar sector
-    SingleShipSurround$PolarSector <- rep (last(BearingSectors),nrow(SingleShipSurround))
-    SingleShipSurround$DistanceSector <- rep (1,nrow(SingleShipSurround))
-    
-    #assign for all surrounding points
-    for (i in 1:(length(BearingSectors)-1)) {
-      SingleShipSurround$PolarSector[between(SingleShipSurround$Bearing, BearingSectors[i], BearingSectors[i+1])] <- BearingSectors[i]
-      SingleShipSurround$DistanceSector[between(SingleShipSurround$DistToOrg, DistanceSectors[i], DistanceSectors[i+1])] <- i
-    }
-    
-    #Add a name to which the Obs is attached
-    SingleShipSurround$Name <- bestSingleShipLoc$Name
-    
-    #Normalize on Latitude to facilitate plotting later on
-    bestSingleShipLoc$normLatitude <- bestSingleShipLoc$Latitude-bestSingleShipLoc$Latitude
-    SingleShipSurround$normLatitude <- SingleShipSurround$Latitude - bestSingleShipLoc$Latitude
-    
-    #To facilitate plotting of actual course in polar plot, it is necessary to construct an adjusted origin course as plot orientation is adjusted to center sector on origin.
-    # also add a Bearing on shift of first sector from North as true course of sector
-    #bestSingleShipLoc$AdjOrigin <- bestSingleShipLoc$Origin-BearingSectors[1]+BearingShift
-    bestSingleShipLoc$AdjOrigin <- bestSingleShipLoc$Origin
-    bestSingleShipLoc$PlotBearing <- BearingSectors[1]
-    
-    #Plot results of ship location interpolation and NO2Obs
-    if (FALSE) {
-      lon = mean(bestSingleShipLoc$Longitude)
-      lat = mean(bestSingleShipLoc$Latitude)
-      oceanmap <- get_map(location = c(lon, lat), zoom =9)
-      g <- ggmap(oceanmap)
-      g+
-        geom_point(data = SingleShipSurround, aes(x = Longitude, y = Latitude, color = DistToOrg), alpha = 0.6, shape = 15, size = 6) +
-        scale_color_gradientn(colours = terrain.colors(7)) +
-        geom_point(data = SingleShipLoc, aes(x = Longitude, y = Latitude, shape = Name)) +
-        geom_point(data = bestSingleShipLoc, aes(x = Longitude, y = Latitude, shape = Name), size = 5) 
-    }
- # }
+  
+  #extract nearest neighbour TROPOMI observations of known AIS locations
+  matches <- nn2(NO2Obs[,1:2], SingleShipLoc[,2:3],1)
+  #combine Obs en Loc
+  SingleShipMatches <- cbind (SingleShipLoc,NO2Obs[matches$nn.idx,])
+  #Calculate differene in Time of datapoint
+  SingleShipMatches$DeltaT <- SingleShipMatches[,1]-SingleShipMatches[,11]
+  
+  # Select the rows with the lowest timedifference
+  best <- which.min(abs(SingleShipMatches$DeltaT))
+  nextbest <- nn2(SingleShipMatches$DeltaT, SingleShipMatches$DeltaT[best],2)$nn.idx[2]
+  
+  #build a df on the best available match for ship location
+  bestSingleShipLoc <- filter (SingleShipLoc, TimeStamp == SingleShipMatches$TimeStamp[best])
+  
+  ###Interpolate to increase matching the right spot
+  #build a data.frame for clarity
+  nrows <- 150
+  SingleShipInterpol <- as.data.frame(matrix(NA,nrows,3))
+  colnames(SingleShipInterpol) <- c("time", "Latitude", "Longitude")
+  
+  #make series to match from
+  SingleShipInterpol$time <- seq (SingleShipMatches$TimeStamp[best], SingleShipMatches$TimeStamp[nextbest], length.out = nrows)
+  SingleShipInterpol$Latitude <- seq (SingleShipMatches$Latitude[best], SingleShipMatches$Latitude[nextbest], length.out = nrows)
+  SingleShipInterpol$Longitude <- seq (SingleShipMatches$Longitude[best], SingleShipMatches$Longitude[nextbest], length.out = nrows)
+  
+  #find the best matching interpolated time
+  idx <- nn2(SingleShipInterpol$time, SingleShipMatches$time[best],1)$nn.idx
+  
+  #Start with original best match, update with new tim, lat and long
+  bestSingleShipLoc$TimeStamp <- SingleShipInterpol$time[idx]
+  bestSingleShipLoc$Latitude <-SingleShipInterpol$Latitude[idx]
+  bestSingleShipLoc$Longitude <- SingleShipInterpol$Longitude[idx]
+  # add Origin as opposite of Heading
+  bestSingleShipLoc$Origin <- ifelse(bestSingleShipLoc$Heading[1]>180,bestSingleShipLoc$Heading[1] - 180,bestSingleShipLoc$Heading[1] + 180)
+  
+  #clean up mess of intermediate vars
+  rm(nrows,best, nextbest, idx, SingleShipInterpol)
+  
+  ############
+  #Match surrounding cells with bestSingleShipLoc, calculate diff in concentrations and plot
+  
+  #first determine which obs are surrounding the ship
+  matches <- nn2(NO2Obs[,1:2], bestSingleShipLoc[,2:3],100)
+  SingleShipSurround <- NO2Obs[matches$nn.idx,]
+  SingleShipSurround$DistToOrg <- distGeo(cbind(SingleShipSurround$Longitude,SingleShipSurround$Latitude), c(bestSingleShipLoc$Longitude, bestSingleShipLoc$Latitude))/1000
+  #determine for each point the difference in Obs conc with the cell where the ship is at >0 means higher than where ship is. 
+  refConc <-  SingleShipSurround$Obs[which(SingleShipSurround$DistToOrg == min(SingleShipSurround$DistToOrg))]
+  SingleShipSurround$DeltaConc <- SingleShipSurround$Obs-refConc
+  # calculate the bearing of each cell surrounding the ship, to later assign sector in polar plot
+  origin <-c(bestSingleShipLoc$Longitude,bestSingleShipLoc$Latitude)
+  SingleShipSurround$Bearing <- sapply(1:nrow(SingleShipSurround), function (x) {bearingRhumb(origin,c(SingleShipSurround$Longitude[x],SingleShipSurround$Latitude[x]))})
+  #calculate a normalized bearing as to orientate axis default as coming from the North
+  SingleShipSurround$normBearing <- SingleShipSurround$Bearing-bestSingleShipLoc$Origin
+  SingleShipSurround$normBearing[SingleShipSurround$normBearing < 0] <- SingleShipSurround$normBearing[SingleShipSurround$normBearing < 0] + 360
+  
+  ##assign Polar sector and Distance sector to the Obs surrounding the ship
+  #YOU MUST USE AN EVEN NUMBER OF SECTORS
+  step <- 360/16
+  BearingSectors <- seq(step/2,360-step/2, by = step)
+  #modify sectors so that centre of a sector will match the ships heading 
+  #THIS IS NOT WORKING FOR THE FIRST AND LAST NORTHERN SECTOR YET
+  nearest <- nn2(BearingSectors, bestSingleShipLoc$Origin,2)
+  dif <- (BearingSectors[nearest$nn.idx[1]]-BearingSectors[nearest$nn.idx[2]])/2
+  BearingShift <- ifelse (dif >= 0, dif - nearest$nn.dists[1], dif + nearest$nn.dists[1])
+  BearingSectors <- BearingSectors + BearingShift
+  #define the sectors with respect to distance
+  DistanceSectors <- seq(min(SingleShipSurround$DistToOrg),max(SingleShipSurround$DistToOrg), length.out = 8)
+  #assign default value for Distance and Polar sector
+  SingleShipSurround$PolarSector <- rep (last(BearingSectors),nrow(SingleShipSurround))
+  SingleShipSurround$DistanceSector <- rep (1,nrow(SingleShipSurround))
+  
+  #assign for all surrounding points
+  for (i in 1:(length(BearingSectors)-1)) {
+    SingleShipSurround$PolarSector[between(SingleShipSurround$Bearing, BearingSectors[i], BearingSectors[i+1])] <- BearingSectors[i]
+    SingleShipSurround$DistanceSector[between(SingleShipSurround$DistToOrg, DistanceSectors[i], DistanceSectors[i+1])] <- i
+  }
+  
+  #Add a name to which the Obs is attached
+  SingleShipSurround$Name <- bestSingleShipLoc$Name
+  
+  #Normalize on Latitude to facilitate plotting later on
+  bestSingleShipLoc$normLatitude <- bestSingleShipLoc$Latitude-bestSingleShipLoc$Latitude
+  SingleShipSurround$normLatitude <- SingleShipSurround$Latitude - bestSingleShipLoc$Latitude
+  
+  #To facilitate plotting of actual course in polar plot, it is necessary to construct an adjusted origin course as plot orientation is adjusted to center sector on origin.
+  # also add a Bearing on shift of first sector from North as true course of sector
+  #bestSingleShipLoc$AdjOrigin <- bestSingleShipLoc$Origin-BearingSectors[1]+BearingShift
+  bestSingleShipLoc$AdjOrigin <- bestSingleShipLoc$Origin
+  bestSingleShipLoc$PlotBearing <- BearingSectors[1]
+  
   #remove datapoints that are too far away
   SingleShipSurround<-filter(SingleShipSurround, DistToOrg < 100)
   
@@ -117,4 +105,33 @@ fCollectSingleShipObs <- function (NO2Obs, Shipdata, ShipName) {
   fCollectSingleShipObsList <- list("SingleShipSurround" = SingleShipSurround, "bestSingleShipLoc" = bestSingleShipLoc)
   return (fCollectSingleShipObsList)
   
+  #########
+  #Plot results of ship location interpolation and NO2Obs
+  if (FALSE) {
+    lon = mean(bestSingleShipLoc$Longitude)
+    lat = mean(bestSingleShipLoc$Latitude)
+    oceanmap <- get_map(location = c(lon, lat), zoom =9)
+    g <- ggmap(oceanmap)
+    g+
+      geom_point(data = SingleShipSurround, aes(x = Longitude, y = Latitude, color = DeltaConc), alpha = 0.6, shape = 15, size = 6) +
+      scale_color_gradientn(colours = terrain.colors(7)) +
+      geom_point(data = SingleShipLoc, aes(x = Longitude, y = Latitude, shape = Name)) +
+      geom_point(data = bestSingleShipLoc, aes(x = Longitude, y = Latitude, shape = Name), size = 5)
+  }
+  ##########
+  #Plot a radial and sector plot of point cloud
+  
+  RadialPlotData<-SingleShipSurround %>%
+    group_by(.dots= c("Name", "PolarSector","DistanceSector")) %>%
+    summarize(meanDeltaConc = mean(DeltaConc, na.rm = TRUE))
+  
+  ggplot(RadialPlotData, aes(fill = meanDeltaConc, xmin =PolarSector, ymin = DistanceSector, xmax = PolarSector+360/16, ymax = DistanceSector+1)) +
+    geom_rect() +
+    #coord_polar(start = bestSingleShipLoc$PlotBearing*pi/180) +
+    coord_polar() +
+    scale_x_continuous(labels = NULL) +
+    scale_fill_gradient2(low = "green"
+                         , mid = "yellow"
+                         , high = "red") +
+    geom_vline(data = bestSingleShipLoc, aes(xintercept = AdjOrigin), size = 1.5)
 }
--- scripts/2_MatchTROPOMIShips.R
+++ scripts/2_MatchTROPOMIShips.R
@@ -9,6 +9,8 @@ library(lubridate)
 library(ggplot2)
 library(cowplot)
 library (geosphere)
+library (parallel)
+
 
 #refer to external functions
 source("scripts/1_collectSingleShipObs.R")
@@ -29,11 +31,20 @@ ShipData$Name <- as.factor(ShipData$Name)
 #sometimes there is double entries ?!
 ShipData <- unique(ShipData)
 
-#Any filters here could also be applied when building the set. rethink everynowand then
+###Any filters here could also be applied when building the set. rethink every nowand then if this is right location
+#if heading is missing remove from data set
 ShipData <- filter(ShipData, !is.na(Heading))
+#if Speed is low no use doing the analysis
 ShipData$Speed <- as.numeric(ShipData$Speed)
 ShipData <- filter(ShipData, Speed > 5)
+#remove any ship with only one AIS point as you cannot interpolate to right time
+NameFreqs <- as.data.frame(table(ShipData$Name))
+NameFreqs <- filter (NameFreqs, Freq > 1)
+ShipData <- filter (ShipData, Name %in% NameFreqs$Var1)
 
+rm(NameFreqs)
+
+############
 #plot all Ships in AISdata
 if (FALSE) {
   lon = mean(ShipData$Longitude)
@@ -45,7 +56,7 @@ if (FALSE) {
 }
 
 ################
-#This part of code will map ships and TROPOMI observations. Allowing to zoom in on one particular ship
+#This plot will map ships and TROPOMI observations. Allowing to zoom in on one particular ship
 ###########
 
 #plot  all Obs and all ShipLocs in a single map 
@@ -62,6 +73,11 @@ if (FALSE) {
     geom_point(data = ShipData, aes(x = Longitude, y = Latitude))
 }
 
+
+#########
+#This chunck will unify Ship locations and TROPOMI observations into sets of observations surrounding individual ships, including intepolation for time matching
+###############
+
 #Build a vector of unique ships to proces
 ShipNames <- unique(ShipData$Name)
 
@@ -71,25 +87,51 @@ fCollectSingleShip <- function(i) {
   return(fCollectSingleShipList)
 }
 
-#call the function for specified number of Ships
-ShipObsList <-sapply (1:16,fCollectSingleShip)
+#for original saplly purposes
+if (FALSE) {
+  #call the function for specified number of Ships
+  ShipObsList <-sapply (1:16,fCollectSingleShip)
+  #restructure list into dataframes
+  index <- seq(1, length (ShipObsList), by =2)
+  SingleShipSurround <- rbindlist(ShipObsList[index], use.names=TRUE)
+  index <- seq(2, length (ShipObsList), by =2)
+  bestSingleShipLoc <- rbindlist(ShipObsList[index], use.names=TRUE) 
+  rm(index)
+}
 
-#restructure list into dataframes
-index <- seq(1, length (ShipObsList), by =2)
-SingleShipSurround <- rbindlist(ShipObsList[index], use.names=TRUE) 
-index <- seq(2, length (ShipObsList), by =2)
-bestSingleShipLoc <- rbindlist(ShipObsList[index], use.names=TRUE) 
-rm(index)
+N <- 180
 
-#remove any ship without any datapoints in the selected area
+#for (parallel(lapply for mac) purposes)
+ifelse (Sys.info()["sysname"] == "Darwin" , ShipObsList <-mclapply (1:N,fCollectSingleShip), ShipObsList <-lapply (1:N,fCollectSingleShip))
+
+for (i in 1:N) {
+  #SingleShipSurroundalt <- ifelse (i == 1, ShipObsListalt[[i]]$SingleShipSurround, rbind(SingleShipSurround, ShipObsListalt[[i]]$SingleShipSurround))
+  SingleShipSurround <- if (i == 1) {ShipObsList[[i]]$SingleShipSurround} else {rbind(SingleShipSurround, ShipObsList[[i]]$SingleShipSurround)}
+  bestSingleShipLoc <- if (i == 1) {ShipObsList[[i]]$bestSingleShipLoc} else {rbind(bestSingleShipLoc, ShipObsList[[i]]$bestSingleShipLoc)}
+}
+
+#remove any ships where distance to nearest datapoint is too large
+#first retrieve closest Observation to Ship
+MinDist <-SingleShipSurround %>%
+  group_by(.dots = c("Name")) %>%
+  summarize(DistToOrg = min(DistToOrg, na.rm = TRUE))
+#filter out any distance longer than given below
+MinDist <- filter(MinDist, DistToOrg <30)
+
+SingleShipSurround <- filter(SingleShipSurround, Name %in% MinDist$Name)
+
+#remove any ship without any surrounding data points from TROPOMI
 bestSingleShipLoc <- filter(bestSingleShipLoc, Name %in% unique(SingleShipSurround$Name))
+
+rm(MinDist)
 
 #########
 #plot Obs and Locs of SingleShips
+###########
 
 lon = mean(bestSingleShipLoc$Longitude)
 lat = mean(bestSingleShipLoc$Latitude)
-oceanmap <- get_map(location = c(lon, lat), zoom =2)
+oceanmap <- get_map(location = c(26.85, 34.93), zoom =6)
 g <- ggmap(oceanmap)
 
 plot1<-g+
@@ -98,11 +140,10 @@ plot1<-g+
   geom_point(data = ShipData, aes(x = Longitude, y = Latitude)) #+
 #  geom_point(data = bestSingleShipLoc, aes(x = Longitude, y = Latitude, shape = Name), size = 5) 
 
-#SingleShipSurround<- filter(SingleShipSurround, Name == "IZKI")
-#bestSingleShipLoc <- filter(bestSingleShipLoc, Name == "IZKI")
-
 ##########
 #Build datastructure to plot in polar, each sector has average of DeltaConc in area
+#############
+
 RadialPlotData<-SingleShipSurround %>%
   group_by(.dots= c("Name", "PolarSector","DistanceSector")) %>%
   summarize(meanDeltaConc = mean(DeltaConc, na.rm = TRUE))
@@ -117,8 +158,9 @@ plot4 <- ggplot() +
 # This plot is a polar sectoral viwe
 plot5 <-ggplot(RadialPlotData, aes(fill = meanDeltaConc, xmin =PolarSector, ymin = DistanceSector, xmax = PolarSector+18, ymax = DistanceSector+1)) +
   geom_rect() +
-  coord_polar(start = bestSingleShipLoc$PlotBearing*pi/180) +
-  facet_grid (cols = vars(Name), scales = NULL) +
+  #coord_polar(start = bestSingleShipLoc$PlotBearing*pi/180) +
+  coord_polar() +
+  facet_wrap (~Name,nrow = 4, scales = NULL) +
   scale_x_continuous(labels = NULL) +
   scale_fill_gradient2(low = "green"
                        , mid = "yellow"
